ebe71c39607aa8435d422be12889252240756cbf
Ricky Cui <ricky.cui@simplybusiness.co.uk>
2020-05-13 13:32:13 +0000

    update orders model

--- models/orders.sql
+++ models/orders.sql
@@ -1,5 +1,5 @@
-select customer.order_id,
-customer.customer_id,
+select orders.order_id,
+orders.customer_id,
 payment.amount
 from {{ref ('stg_jaffle_shop_orders')}} orders
 left join {{ ref('payment')}} payment on orders.order_id = payment."orderID"
