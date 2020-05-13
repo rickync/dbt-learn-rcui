d9fc2e721ba78c84e444773158679416dff3fc22
Ricky Cui <ricky.cui@simplybusiness.co.uk>
2020-05-13 21:48:07 +0000

    updates

--- models/core/dim_orders.sql
+++ models/core/dim_orders.sql
@@ -2,4 +2,4 @@ select orders.order_id,
 orders.customer_id,
 payment.amount
 from {{ref ('stg_jaffle_shop_orders')}} orders
-left join {{ ref('payment')}} payment on orders.order_id = payment."orderID"
+left join {{ ref('stg_stripe_payment')}} payment on orders.order_id = payment.order_id
--- models/core/fct_customers.sql
+++ models/core/fct_customers.sql
@@ -3,6 +3,6 @@ customers.first_name,
 customers.last_name,
 sum(orders.amount) lifetime_value
 from {{ ref('stg_jaffle_shop_customers') }} customers
-left join {{ ref('orders') }} orders on customers.customer_id=orders.customer_id
+left join {{ ref('dim_orders') }} orders on customers.customer_id=orders.customer_id
 group by 1,2,3
 order by 1
