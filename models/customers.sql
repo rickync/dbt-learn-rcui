c7462b509eae9e76fe9c1781d3fab134e9631e9f
Ricky Cui <ricky.cui@simplybusiness.co.uk>
2020-05-13 16:29:44 +0000

    alias column

--- models/customers.sql
+++ models/customers.sql
@@ -1,7 +1,8 @@
 select customers.customer_id,
 customers.first_name,
 customers.last_name,
-sum(orders.amount)
+sum(orders.amount) lifetime_value
 from {{ ref('stg_jaffle_shop_customers') }} customers
 left join {{ ref('orders') }} orders on customers.customer_id=orders.customer_id
 group by 1,2,3
+order by 1
