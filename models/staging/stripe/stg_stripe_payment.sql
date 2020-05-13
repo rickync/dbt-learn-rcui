95d2e860a07382c9844f9661dc6fa9a30ea7bc15
Ricky Cui <ricky.cui@simplybusiness.co.uk>
2020-05-13 22:05:37 +0000

    update

--- models/staging/stripe/stg_stripe_payment.sql
+++ models/staging/stripe/stg_stripe_payment.sql
@@ -1,5 +1,18 @@
-select ID,
-"orderID",
-"paymentMethod",
-AMOUNT
-from raw.stripe.payment
+with source_payment as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+renamed_payment as (
+
+    select
+        id,
+        "orderID" as order_id,
+        "paymentMethod" as payment_method,
+        amount
+    from source_payment
+
+)
+
+select * from renamed_payment
